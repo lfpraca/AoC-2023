2443bb8aec9fb87d84e355a5ff65ac24da9ba7b7
lfpraca <luisfelippe@protonmail.com>
2023-12-09 13:15:44 -0300

    Solves day 9

---
+++ day9/ch2/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let input = include_str!("input.txt");
+
+    let res: i32 = input.lines().map(extrapolate).sum();
+
+    println!("{}", res);
+}
+
+fn extrapolate(line: &str) -> i32 {
+    let mut iterations: Vec<Vec<i32>> = Vec::new();
+    iterations.push(
+        line.split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<_>>(),
+    );
+
+    while !iterations.last().unwrap().iter().all(|&x| x == 0) {
+        iterations.push(
+            iterations
+                .last()
+                .unwrap()
+                .windows(2)
+                .map(|a| a[1] - a[0])
+                .collect::<Vec<i32>>(),
+        );
+    }
+
+    iterations
+        .iter()
+        .rev()
+        .map(|x| x[0])
+        .fold(0, |acc, x| x - acc)
+}
